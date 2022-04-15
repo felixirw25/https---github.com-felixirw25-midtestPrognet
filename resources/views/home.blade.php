ef63711b54c5d0ba62e3234d6afff1f2884cce9e
felixirw25 <felixirwanto25@gmail.com>
2022-04-15 23:50:44 +0800

    tambah logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,14 @@
 
 @section('contents')
 <!--<div class="bg-image" id="bg-image"></div>-->
-<h1 id="beranda">Ubah Beranda</h1>
+<h1 id="beranda">Beranda</h1>
+<img src="{{url('/images/myimage.jpg')}}" alt="Logo">
+<p style="text-align: justify"><i>Integrated Civil Report</i> (ICIPORT) adalah sebuah solusi terintegrasi untuk menyampaikan dan menanggapi keluhan
+    dari masyarakat. Masyarakat mengeluhkan permasalahan di daerah mereka yang belum sama
+    sekali diperhatikan oleh pemerintah, sedangkan untuk sekedar menyampaikan
+    keluhan saja sangat merepotkan dari segi pelayanan dan tindakan. Maka
+    diperlukanlah solusi terintegrasi untuk menyampaikan dan menanggapi keluhan
+    tersebut.
+</p>
 
 @endsection
